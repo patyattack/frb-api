1adcc4f17b138bbfd9dd767489ebc392a13a6d02
Nico Ramirez <45040383+patyattack@users.noreply.github.com>
2019-07-19 14:13:32 -0300

    updating database

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
+      host : 'postgresql-colorful-12341',
       user : 'postgres',
-      password : 'tuco',
+      password : '',
       database : 'brain'
     }
   });
