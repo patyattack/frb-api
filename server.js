f0109f26cbedf24a23fbbe550cbe2fe18736a0f0
Nico Ramirez <45040383+patyattack@users.noreply.github.com>
2019-07-19 13:27:58 -0300

    changing response

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => { res.send(database.users); });
+app.get('/', (req, res) => { res.send('it is working'); });
 
 app.post('/signin', signin.handleSignin(db, bcrypt));
 
